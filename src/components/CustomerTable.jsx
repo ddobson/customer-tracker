c47c8bcbe7722ac63a4c097855d8090bde8d2881
ddobson <ddobson445@gmail.com>
2017-04-18 12:43:43 -0400

    CustomerTable is a class
    - Change CustomerTable from a stateless functional component to a class
    - CustomerTable has state

--- src/components/CustomerTable.jsx
+++ src/components/CustomerTable.jsx
@@ -2,20 +2,32 @@ import React from 'react';
 
 import CustomerRow from './CustomerRow';
 
-const CustomerTable = (props) => {
-  return (
-    <div className="col-xs-12 col-md-6">
-      <div className="content-wrap center-text">
-        <div className="content-header">
-          <h2>Customers</h2>
-        </div>
-          <div className="content-item">
-            <p className="new-customer-cta customer-row" onClick={ () => props.setNewStatus(true) }>Add New Customer</p>
+class CustomerTable extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      listedCustomers: this.props.customers
+    };
+  }
+
+  render() {
+    return (
+      <div className="col-xs-12 col-md-6">
+        <div className="content-wrap center-text">
+          <div className="content-header">
+            <h2>Customers</h2>
           </div>
-          { props.customers.map((customer, i) => <CustomerRow key={`customer-${i}`} customer={customer} showCustomer={ props.showCustomer }/>) }
+            <div className="content-item">
+              <p className="new-customer-cta customer-row" onClick={ () => this.props.setNewStatus(true) }>Add New Customer</p>
+            </div>
+            { this.state.listedCustomers.map((customer, i) =>
+              <CustomerRow key={`customer-${i}`} customer={customer} showCustomer={ this.props.showCustomer }/>)
+            }
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default CustomerTable;
